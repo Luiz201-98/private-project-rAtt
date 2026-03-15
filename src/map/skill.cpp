3967d8881321c4a8271cf0b2f217c7ac531036d2
dieinthesky <dieintheskyl9@gmail.com>
2026-03-14 21:32:32 -0300

    Modificação do ME+ SkillDB

--- src/map/skill.cpp
+++ src/map/skill.cpp
@@ -14372,14 +14372,14 @@ TIMER_FUNC(skill_castend_pos){
 			if (md->skill_idx >= 0 && md->db->skill[md->skill_idx]->emotion >= ET_SURPRISE && md->db->skill[md->skill_idx]->emotion < ET_MAX)
 				clif_emotion(*src, static_cast<emotion_type>(md->db->skill[md->skill_idx]->emotion));
 		}  
-		if (skill_get_type(ud->skill_id) == BF_MAGIC && ud->skill_id != SA_LANDPROTECTOR) {  
-			if (map_getcell(src->m, ud->skillx, ud->skilly, CELL_CHKLANDPROTECTOR)) {  
-				if (sd) {  
-					clif_skill_fail(*sd, ud->skill_id, USESKILL_FAIL);  
-				}  
-				break; // Falha o lançamento  
+	if (skill_get_type(ud->skill_id) == BF_MAGIC && ud->skill_id != SA_LANDPROTECTOR) {  
+		if (map_getcell(src->m, ud->skillx, ud->skilly, CELL_CHKLANDPROTECTOR)) {  
+			if (sd) {  
+				clif_skill_fail(*sd, ud->skill_id, USESKILL_FAIL);  
 			}  
-		}
+			break; // Falha o lançamento  
+		}  
+	}
 
 		if (!skill_pos_maxcount_check(src, ud->skillx, ud->skilly, ud->skill_id, ud->skill_lv, src->type, true))
 			break;
